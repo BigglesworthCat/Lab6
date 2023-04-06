5ae7d4950e5f456042633562085f7b1b5e244048
Dmytro Kriuchkov <kriuchkovdima@gmail.com>
2023-04-06 03:25:02 +0300

    Design & refactoring.

--- main.py
+++ main.py
@@ -4,18 +4,27 @@ from tkinter import filedialog
 import pandas
 from pandastable import Table, TableModel
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 from cognitive_matrix import Stability
 from impulse_modeling import ImpulseProcess
 
+
 class Application(Tk):
     def __init__(self):
         super().__init__()
-        self.title('Лабораторна робота 4')
-        # self.resizable(False, False)
+        self.title('Лабораторна робота 6')
+        self.resizable(False, False)
+
+        self.options_frame = Frame(self)
+        self.options_frame.pack(side='left', fill='both', expand=True)
+
+        self.matrices_frame = Frame(self)
+        self.matrices_frame.pack(side='right', fill='both', expand=True)
 
         # 'Файли'
-        self.files_label_frame = LabelFrame(self, text='Файли:')
+        self.files_label_frame = LabelFrame(self.options_frame, text='Файли:')
         self.files_label_frame.grid(row=0, column=0, columnspan=3, sticky='NWES', padx=5, pady=5, ipadx=5, ipady=5)
 
         self.input_matrix_label = Label(self.files_label_frame, text='Файл з матрицею:')
@@ -26,41 +35,42 @@ class Application(Tk):
         self.input_matrix_path_entry = Entry(self.files_label_frame, textvariable=self.input_matrix_path_var)
         self.input_matrix_path_entry.grid(row=0, column=1, sticky='WE', padx=5, pady=2)
 
-        self.browse_button = Button(self.files_label_frame, text='...', command=self.open_input_matrix)
-        self.browse_button.grid(row=0, column=2, sticky='W', padx=5, pady=2)
+        self.browse_input_button = Button(self.files_label_frame, text='...', command=self.open_input_matrix)
+        self.browse_input_button.grid(row=0, column=2, sticky='WE', padx=5, pady=2)
 
         self.output_matrix_label = Label(self.files_label_frame, text='Файл з результатами:')
         self.output_matrix_label.grid(row=1, column=0, sticky='E', padx=5, pady=2)
 
-        self.output_matrix_path = StringVar()
-        self.output_matrix_path.set('')
-        self.output_matrix_path_entry = Entry(self.files_label_frame, textvariable=self.output_matrix_path)
+        self.output_matrix_path_var = StringVar()
+        self.output_matrix_path_var.set('')
+        self.output_matrix_path_entry = Entry(self.files_label_frame, textvariable=self.output_matrix_path_var)
         self.output_matrix_path_entry.grid(row=1, column=1, sticky='WE', padx=5, pady=2)
 
-        self.output_button = Button(self.files_label_frame, text='...', command=self.open_input_matrix)
-        self.output_button.grid(row=1, column=2, sticky='W', padx=5, pady=2)
+        self.browse_output_button = Button(self.files_label_frame, text='...', command=self.open_output_matrix)
+        self.browse_output_button.grid(row=1, column=2, sticky='WE', padx=5, pady=2)
 
         # 'Імпульс'
-        self.impulse_label_frame = LabelFrame(self, text='Імпульс:')
+        self.impulse_label_frame = LabelFrame(self.options_frame, text='Імпульс:')
         self.impulse_label_frame.grid(row=1, column=0, columnspan=3, sticky='NWES', padx=5, pady=5, ipadx=5, ipady=5)
 
         self.iterations_label = Label(self.impulse_label_frame, text='Кількість ітерацій:')
-        self.iterations_label.grid(row=0, column=0, sticky='WE', padx=5, pady=2)
+        self.iterations_label.grid(row=0, column=0, sticky='E', padx=5, pady=2)
+
         self.iterations_var = StringVar()
-        self.iterations_var.set('3')
-        self.iterations_spinbox = Spinbox(self.impulse_label_frame, from_=1, to=20, width=5,
+        self.iterations_var.set('5')
+        self.iterations_spinbox = Spinbox(self.impulse_label_frame, from_=1, to=20, width=30,
                                           textvariable=self.iterations_var)
-        self.iterations_spinbox.grid(row=0, column=1, sticky='WE', padx=5, pady=2)
+        self.iterations_spinbox.grid(row=0, column=1, columnspan=2, sticky='WE', padx=5, pady=2)
 
         self.calculate_button = Button(self.impulse_label_frame, text='Змоделювати',
                                        command=self.model_impulse,
                                        bg='red',
                                        fg='white'
                                        )
-        self.calculate_button.grid(row=1, column=0, columnspan=3, sticky='NWES', padx=5, pady=5, ipadx=5, ipady=5)
+        self.calculate_button.grid(row=1, column=0, columnspan=3, sticky='WE', padx=5, pady=5, ipadx=5, ipady=5)
 
         # 'Стійкість'
-        self.sustainability_label_frame = LabelFrame(self, text='Стійкість:')
+        self.sustainability_label_frame = LabelFrame(self.options_frame, text='Стійкість:')
         self.sustainability_label_frame.grid(row=2, column=0, columnspan=3, sticky='NWES', padx=5, pady=5, ipadx=5,
                                              ipady=5)
 
@@ -72,15 +82,20 @@ class Application(Tk):
         self.check_sustainability_button.grid(row=0, column=0, sticky='WE', padx=5, pady=5, ipadx=5, ipady=5)
 
         self.values_sustainability_checkbutton = Checkbutton(self.sustainability_label_frame,
-                                                             text='Стійкість за значенням')
+                                                             text='Стійкість за значенням', state='disabled')
+        self.values_sustainability_checkbutton.config(disabledforeground="black")
         self.values_sustainability_checkbutton.grid(row=1, column=0, sticky='W', padx=5, pady=2)
 
         self.disturbance_sustainability_checkbutton = Checkbutton(self.sustainability_label_frame,
-                                                                  text='Стійкість за збуренням')
+                                                                  text='Стійкість за збуренням', state='disabled')
+        self.disturbance_sustainability_checkbutton.config(disabledforeground="black")
+
         self.disturbance_sustainability_checkbutton.grid(row=2, column=0, sticky='W', padx=5, pady=2)
 
         self.structure_sustainability_checkbutton = Checkbutton(self.sustainability_label_frame,
-                                                                text='Структурна стійкість')
+                                                                text='Структурна стійкість', state='disabled')
+        self.structure_sustainability_checkbutton.config(disabledforeground="black")
+
         self.structure_sustainability_checkbutton.grid(row=3, column=0, sticky='W', padx=5, pady=2)
 
         ## 'Цикли'
@@ -110,122 +125,100 @@ class Application(Tk):
         self.eigenvalues_listbox.grid(row=1, column=0, columnspan=2, sticky='WE', padx=5, pady=2)
 
         # 'Оригінальна матриця'
-        self.adjacent_matrix_label_frame = LabelFrame(self, text='Матриця суміжності:')
-        self.adjacent_matrix_label_frame.grid(row=0, column=3, columnspan=3)
+        self.adjacent_matrix_label_frame = LabelFrame(self.matrices_frame, text='Матриця суміжності:')
+        self.adjacent_matrix_label_frame.grid(row=0, column=3, rowspan=2)
 
-        self.adjacent_matrix = Table(self.adjacent_matrix_label_frame)
-        self.adjacent_matrix.show()
-        self.adjacent_matrix.model.df = TableModel.getSampleData()
+        self.adjacent_matrix_table = Table(self.adjacent_matrix_label_frame, height=231)
+        self.adjacent_matrix_table.show()
 
-        # 'Імпульси'
-        self.impulse_matrix_label_frame = LabelFrame(self, text='Ітерації:')
-        self.impulse_matrix_label_frame.grid(row=1, column=3, columnspan=3)
+        # 'Ітерації'
+        self.impulse_matrix_label_frame = LabelFrame(self.matrices_frame, text='Ітерації:')
+        self.impulse_matrix_label_frame.grid(row=2, column=3, rowspan=2)
 
-        self.impulse_matrix = Table(self.impulse_matrix_label_frame)
-        
-        self.impulse_matrix.updateModel(TableModel(pd.DataFrame([[1]*8]*8)))
-        self.impulse_matrix.show()
+        self.impulse_matrix_table = Table(self.impulse_matrix_label_frame, height=231)
+        self.impulse_matrix_table.show()
 
         # 'Вектор збурення'
-        self.disturbance_vector_label_frame = LabelFrame(self, text='Вектор збурення:')
-        self.disturbance_vector_label_frame.grid(row=2, column=3, columnspan=3)
+        self.disturbance_vector_label_frame = LabelFrame(self.matrices_frame, text='Вектор збурення:')
+        self.disturbance_vector_label_frame.grid(row=4, column=3, rowspan=2)
 
-
-        disturbance_vector = pd.DataFrame([[0]]*8)
-        self.disturbance_vector = Table(self.disturbance_vector_label_frame)
-        self.disturbance_vector.updateModel(TableModel(disturbance_vector))
-        
-        self.disturbance_vector.show()
+        self.disturbance_vector_table = Table(self.disturbance_vector_label_frame, height=231)
+        self.disturbance_vector_table.show()
 
     def open_input_matrix(self):
         input_matrix_path = filedialog.askopenfilename(title='Open a File',
-                                                  filetypes=(("Excel files", ".*xlsx"), ("All Files", "*.")))
+                                                       filetypes=(("Excel files", ".*xlsx"), ("All Files", "*.")))
         self.input_matrix_path_var.set(input_matrix_path)
-        self.input_df = pandas.read_excel(input_matrix_path)
-        self.input_df = self.input_df.drop(columns=['Unnamed: 0'])
-        self.adjacent_matrix.updateModel(TableModel(self.input_df))
-        self.adjacent_matrix.redraw()
-        
-        self.disturbance_vector_df = pd.DataFrame([[0]]*self.input_df.shape[1])
-        self.disturbance_vector.updateModel(TableModel(self.disturbance_vector_df))
-        self.disturbance_vector.redraw()
-        
+        self.adjacent_matrix = pandas.read_excel(input_matrix_path, index_col=0)
+        self.adjacent_matrix_table.updateModel(TableModel(self.adjacent_matrix))
+        self.adjacent_matrix_table.redraw()
+
+        self.disturbance_vector = pd.DataFrame([[0]] * self.adjacent_matrix.shape[1], index=self.adjacent_matrix.index)
+        self.disturbance_vector_table.updateModel(TableModel(self.disturbance_vector))
+        self.disturbance_vector_table.redraw()
+
+    def open_output_matrix(self):
+        output_matrix_path = filedialog.askopenfilename(title='Open a File',
+                                                        filetypes=(("Excel files", ".*xlsx"), ("All Files", "*.")))
+        self.output_matrix_path_var.set(output_matrix_path)
+
     def check_sustainability(self):
-        self.check_stability()
+        adjacent_matrix = self.adjacent_matrix.copy()
+        for i, col_name in enumerate(self.adjacent_matrix.columns):
+            adjacent_matrix.rename({col_name: i}, axis='columns', inplace=True)
+        self.stability = Stability(adjacent_matrix)
 
-    def model_impulse(self):
-        self.value_impulse_modeling()
-        
-    def check_stability(self):
-        """Перевіряємо матрицю на стійкість та виводимо результати у інтерфейс."""
-        input_df = self.input_df.copy()
-        for i,col_name in enumerate(self.input_df.columns):
-            input_df.rename({col_name:i},axis='columns',inplace=True)            
-        self.stability = Stability(input_df)
-
-        def bull_converter(bull_term):
-            return 'так!' if bull_term else 'ні!'
         if self.stability.structure_stability:
             self.values_sustainability_checkbutton.select()
         else:
             self.values_sustainability_checkbutton.deselect()
 
         if self.stability.value_stability:
-            self.structure_sustainability_checkbutton.select()
-        else:
-            self.structure_sustainability_checkbutton.deselect()
-
-        if self.stability.value_stability:
             self.disturbance_sustainability_checkbutton.select()
         else:
             self.disturbance_sustainability_checkbutton.deselect()
 
-        # self.eigenvalues_listbox.clear
-        for ev in self.stability.eigenvalues:
-            self.eigenvalues_listbox.insert(END,'{0:.2f} {1} {2:.2f}i'.format(ev.real, '+-'[int(ev.imag < 0)], abs(ev.imag)))
-        # self.max_eigenvalues_entry.clear
-        self.max_eigenvalues_entry.insert(END,f'{self.stability.spectral_radius:.2f}')
-        self.cycles_number_entry.insert(END,str(len(self.stability.graph.pair_cycles)))
+        if self.stability.value_stability:
+            self.structure_sustainability_checkbutton.select()
+        else:
+            self.structure_sustainability_checkbutton.deselect()
 
-        self.cycles_listbox.clear()
-        
-        rofl_list = []
+        self.cycles_number_entry.delete(0, END)
+        self.cycles_number_entry.insert(END, str(len(self.stability.graph.pair_cycles)))
+
+        items_list = []
         for pc in self.stability.graph.pair_cycles:
             pc.append(pc[0])
-            pc = '~>'.join(f'e{e}' for e in pc)
-            rofl_list.append(pc)
+            pc = '~>'.join(f'{self.adjacent_matrix.columns[e]}' for e in pc)
+            items_list.append(pc)
+        items_list = sorted(items_list, key=len)
+        for pc in items_list:
+            self.cycles_listbox.insert(END, pc)
 
-        rofl_list = sorted(rofl_list, key=len)
-        for pc in rofl_list:
-            self.cycles_listbox.insert(END,pc)
-            
-        # self.print_eigenvalues()
-        # self.print_pair_cycles()
-    def value_impulse_modeling(self):
-        """Створити моделювання у табличному вигляді."""
-        import numpy as np
-        import matplotlib.pyplot as plt
+        self.max_eigenvalues_entry.delete(0, END)
+        self.max_eigenvalues_entry.insert(END, f'{self.stability.spectral_radius:.2f}')
+
+        self.eigenvalues_listbox.delete(0, END)
+        for ev in self.stability.eigenvalues:
+            self.eigenvalues_listbox.insert(END, '{0:.2f} {1} {2:.2f}i'.format(ev.real, '+-'[int(ev.imag < 0)],
+                                                                               abs(ev.imag)))
+
+    def model_impulse(self):
         n_steps = int(self.iterations_var.get())
-        q_impulses = pd.DataFrame(data=np.zeros((self.input_df.shape[0], n_steps+1)), index=self.input_df.index,
-                                columns=list(range(n_steps + 1)))
-        
-        q_impulses.iloc[:, 0] = self.disturbance_vector_df.iloc[:, 0]
-        print(q_impulses.shape,self.input_df.shape)
-        ip = ImpulseProcess(self.input_df, q_impulses.iloc[:, 0])
-        ip.impulse_modeling(q_impulses, n_steps)
+        q_impulses = pd.DataFrame(data=np.zeros((self.adjacent_matrix.shape[0], n_steps + 1)), index=self.adjacent_matrix.columns,
+                                  columns=list(range(n_steps + 1)))
 
+        q_impulses.iloc[:, 0] = self.disturbance_vector.iloc[:, 0].astype(np.float)
+        print(q_impulses.shape, self.adjacent_matrix.shape)
+        ip = ImpulseProcess(self.adjacent_matrix, q_impulses.iloc[:, 0])
+        ip.impulse_modeling(q_impulses, n_steps)
         x = ip.x
-        # df_output = pd.DataFrame(np.zeros(len(self.adjacency_matrix.index,self.n_model_steps+1)))
-        # for i in range(len(self.adjacency_matrix.index)):
-        #     for j in range(self.n_model_steps+1):
-        #         # item = QtWidgets.QTableWidgetItem(f'{x.iloc[i, j]:.2f}')
-        #         # self.x_table.setItem(i, j, item)
-        #         df_output.iloc[i,j]
-        self.impulse_matrix.updateModel(TableModel(x))
-        self.impulse_matrix.redraw()
+        self.impulse_matrix_table.updateModel(TableModel(x))
+        self.impulse_matrix_table.redraw()
         x.T.plot()
         plt.show()
-    
+
+
 if __name__ == "__main__":
     application = Application()
     application.mainloop()
